8d7c1333200b9c9e874e9ce0f8730186822c3399
Shen Tianqi <1055862487@qq.com>
2023-03-20 16:27:29 +0800

    基本架构

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# DDlogOutputProcesser readme file
+## 快速指南
+### 编译运行
+```shell
+mvn clean compile assembly:single
+```
--- src/main/java/cn/edu/nju/stq/Main.java
+++ src/main/java/cn/edu/nju/stq/Main.java
@@ -1,5 +1,41 @@
-package cn.edu.nju.stq;public class Main {
+package cn.edu.nju.stq;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.*;
+
+@Slf4j
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        run(args);
+    }
+
+    private static Options buildOption() {
+        return new Options()
+                .addOption("h", "打印帮助信息")
+                .addOption("i", "input", true, "输入的压缩文件");
+    }
+
+    private static void run(String[] a) {
+        CommandLineParser parser = new DefaultParser();
+        Options options = buildOption();
+        try {
+            CommandLine commandLine = parser.parse(options, a);
+            if (commandLine.hasOption('h') || !commandLine.hasOption('i')) {
+                printHelpInfo(options);
+            } else {
+                log.info("input file: {}", commandLine.getOptionValue('i'));
+                Processer processer = new Processer();
+                processer.readInfoFromZip(commandLine.getOptionValue('i'));
+                processer.printRes();
+            }
+        } catch (ParseException e) {
+            printHelpInfo(options);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printHelpInfo(Options o) {
+        new HelpFormatter().printHelp("java -jar DDlogOutputProcesser-0.1-SNAPSHOT.jar", o, true);
     }
 }
